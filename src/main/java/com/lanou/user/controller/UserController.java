ca60184f1b127523116b08482c406fab6e4b4c74
dllo <765557761@qq.com>
2017-11-09 18:01:30 +0800

    显示验证码

--- src/main/java/com/lanou/user/controller/UserController.java
+++ src/main/java/com/lanou/user/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.lanou.user.controller;
+import com.lanou.user.bean.SysUser;
 import com.lanou.user.exception.CustomException;
 import com.lanou.user.service.UserService;
+import com.lanou.utils.VerifyCode;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Created by dllo on 17/11/8.
@@ -34,10 +43,35 @@ public class UserController {
     public String welcome(){
         return "welcome";
     }
+    // 退出/切换用户
     @RequestMapping(value = "/logout")
     public void logout(){
         SecurityUtils.getSubject().logout();
     }
+    //个人信息
+    @ResponseBody
+    @RequestMapping(value = "/myself")
+    public SysUser myself(@RequestParam("loginName") String username){
+        return service.findSysByName(username);
+    }
+    //验证码
+    @ResponseBody
+    @RequestMapping(value = "/verifyCode")
+    public void verifyCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 创建工具类对象p
+        VerifyCode verifyCode = new VerifyCode();
+        // 验证码工具生成图片对象
+        BufferedImage image = verifyCode.getImage();
+
+        // 将验证码保存到session中-verifyCode.getText()
+        request.getSession().setAttribute("verifyCode",verifyCode.getText());
+
+        // 获得response对象的输出流-用于图像的写入
+        OutputStream os = response.getOutputStream();
+        // 将图片对象映射到输出流中
+        VerifyCode.output(image,os);
+    }
+
 
     @RequestMapping(value = "/loginsubmit",method = RequestMethod.POST)
     public String login(HttpServletRequest request) throws Exception {
