67eeb8f96316fdcf70ca1f586c789eaab6894127
dllo <765557761@qq.com>
2017-11-09 17:02:56 +0800

    登录认证+退出登录

--- src/main/java/com/lanou/user/controller/UserController.java
+++ src/main/java/com/lanou/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.lanou.user.controller;
 import com.lanou.user.exception.CustomException;
 import com.lanou.user.service.UserService;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.springframework.stereotype.Controller;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-
 
 /**
  * Created by dllo on 17/11/8.
@@ -20,11 +20,26 @@ public class UserController {
     private UserService service;
 
     @RequestMapping(value = "/toLoginPage")
-    public String index(){
+    public String login1(){
         return "login";
     }
+    @RequestMapping(value = "/index")
+    public String index(){
+        if (SecurityUtils.getSubject().isAuthenticated()){
+            SecurityUtils.getSubject().logout();
+        }
+        return "index";
+    }
+    @RequestMapping(value = "/welcome")
+    public String welcome(){
+        return "welcome";
+    }
+    @RequestMapping(value = "/logout")
+    public void logout(){
+        SecurityUtils.getSubject().logout();
+    }
 
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    @RequestMapping(value = "/loginsubmit",method = RequestMethod.POST)
     public String login(HttpServletRequest request) throws Exception {
         String exceptionClassName = (String) request.getAttribute("shiroLoginFailure");
         if (exceptionClassName.equals(UnknownAccountException.class.getName())) {
